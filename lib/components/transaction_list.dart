f874872166f4084ffa3dc6f8683a7ff796f2b824
leoCardosoDev <leocardosodev@gmail.com>
2023-11-27 14:53:25 -0300

    refactor: move singlechildscrollview

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,51 +9,56 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-            children: transactions
-                .map((tr) => Card(
-                      elevation: 5,
-                      child: Row(children: [
-                        Container(
-                          margin: const EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 10,
-                          ),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                            color: Colors.purple,
-                          )),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            'R\$ ${tr.value.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tr.title,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+                children: transactions
+                    .map((tr) => Card(
+                          elevation: 5,
+                          child: Row(children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                color: Colors.purple,
+                              )),
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                'R\$ ${tr.value.toStringAsFixed(2)}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.purple,
+                                ),
                               ),
                             ),
-                            Text(
-                              DateFormat('d MMM y').format(tr.date),
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  tr.title,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat('d MMM y').format(tr.date),
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                  ),
+                                )
+                              ],
                             )
-                          ],
-                        )
-                      ]),
-                    ))
-                .toList(),
-          );
+                          ]),
+                        ))
+                    .toList(),
+              ),
+      ),
+    );
   }
 }
